765bf69e8de3732cf0534662544014f0753bc90e
KKA-0 <karanyobro@gmail.com>
2024-03-20 18:33:20 +0530

    Feature: Overview KanbanBoard

---
+++ Client/src/components/Dashboard/MainBody/Pages/cards/overview.kanbanboard.colmn.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import components from './../overview/components/components.module.css'
+import Cards from './overview.kanbanboard.card'
+import { Droppable } from "react-beautiful-dnd";
+const Colmn = ({ title, tasks, colmn }) => {
+
+  function todoDiv(){
+    if(colmn === "todo"){
+        return components.todo_Div
+    }else if(colmn === "inProgress"){
+        return components.doin_Div
+    }else if(colmn === "done"){
+        return components.done_Div
+    }
+  }
+  function todo_Div_title(){
+    if(colmn === "todo"){
+        return components.todo_Div_title
+    }else if(colmn === "inProgress"){
+        return components.doin_Div_title
+    }else if(colmn === "done"){
+        return components.done_Div_title
+    }
+  }
+
+
+  return (
+    <div className={todoDiv()}>
+          <div className={todo_Div_title()}>
+            <span>{title}</span>
+          </div>
+          <Droppable droppableId={colmn}>
+            {(provided, snapshot) => (
+                <div className={components.Div_cards}
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    isDraggingOver={snapshot.isDraggingOver}
+                >
+                    {tasks.map((task, index) => (
+                            <Cards key={task.task_id} colm={colmn} task={task} index={index} />
+                        ))}
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
+    </div>
+  )
+}
+
+export default Colmn
--- Client/src/components/Dashboard/MainBody/Pages/overview/components/KanbanBoard.overview.jsx
+++ Client/src/components/Dashboard/MainBody/Pages/overview/components/KanbanBoard.overview.jsx
@@ -1,43 +1,38 @@
 import components from './components.module.css'
+import { DragDropContext } from "react-beautiful-dnd";
 import dashboard from './../../../../Dashboard.module.css'
+import Colmn from '../../cards/overview.kanbanboard.colmn'
+import useTask from "./../../../../../../hooks/useTask.redux"
+import { moveTask } from "../../../../../../features/taskSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const KarbanBoard = () => {
+  const dispatch = useDispatch()
+  const user_id = useSelector((state) => state.user.id)
+  const taskData = useTask()
+
+  const handleDragEnd = (result) => {
+    // console.log(result)
+    if(result.destination === null || result.destination === undefined) {
+        return 0;
+    }else{
+        const data = {
+            user_id: user_id,
+            source: result.source.droppableId,
+            destination: result.destination.droppableId,
+            index: result.destination.index,
+            task_id: result.draggableId
+        }
+        dispatch(moveTask(data))
+    }
+};
   return (
       <div className={dashboard.kanbanBoarddiv}>
-        <div className={components.todo_Div}>
-          <div className={components.todo_Div_title}>
-            <span>ToDo</span>
-          </div>
-          <div className={components.Div_cards}>
-            <div draggable="true" className={components.todo_Div_card}>
-                <div className={components.card_title_div}>
-                    <span>asdf</span>
-                </div>
-                <div className={components.card_DateTime_div}>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div className={components.doin_Div}>
-          <div className={components.doin_Div_title}>
-            <span>In-Progress</span>
-          </div>
-          <div className={components.Div_cards}>
-            <div className={components.todo_Div_card}>
-
-            </div>
-          </div>
-        </div>
-        <div className={components.done_Div}>
-          <div className={components.done_Div_title}>
-            <span>Done</span>
-          </div>
-          <div className={components.Div_cards}>
-            <div className={components.todo_Div_card}>
-
-            </div>
-          </div>
-        </div>
+        <DragDropContext onDragEnd={handleDragEnd}>
+              <Colmn title={"TO DO"} tasks={taskData.todo} colmn={"todo"}/>
+              <Colmn title={"IN PROGRESS"} tasks={taskData.inProgress} colmn={"inProgress"} />
+              <Colmn title={"DONE"} tasks={taskData.done} colmn={"done"} />
+        </DragDropContext>
       </div>
   )
 }
