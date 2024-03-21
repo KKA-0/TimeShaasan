cb68374fc21a361f9f544cb7bdb1006f54aeb41b
KKA-0 <karanyobro@gmail.com>
2024-03-21 14:59:43 +0530

    Feature: Sidebar Menu for Mobile

--- Client/src/components/Dashboard/sideBar/SideBar.jsx
+++ Client/src/components/Dashboard/sideBar/SideBar.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
-import dashboard from '../Dashboard.module.css'
 import { MdSpaceDashboard } from "react-icons/md";
 import { BsKanban } from "react-icons/bs";
 import { GoChecklist } from "react-icons/go";
 import { CgTimer } from "react-icons/cg";
 import { IoMdSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
-import sidebar from './sidebar.module.css'
+import sidebar from './sidebar.module.scss'
 const SideBar = () => {
   return (
     <>
-        <div className={dashboard.sidebarDiv}>
-        <div className={dashboard.kanbanOption}>
-          <Link className={sidebar.a} to="/overview"><MdSpaceDashboard color="white" size="2em" className={dashboard.icon}/> <span className={dashboard.siderbarOption}>Overview</span></Link>
+        <div className={sidebar.sidebarDiv}>
+        <div className={sidebar.kanbanOption}>
+          <Link className={sidebar.a} to="/overview"><MdSpaceDashboard color="white" size="2em" className={sidebar.icon}/> <span className={sidebar.siderbarOption}>Overview</span></Link>
         </div>
-        <div draggable='true' className={dashboard.kanbanOption}>
-          <Link className={sidebar.a} to="/kanbanboard"><BsKanban color="white" size="2em" className={dashboard.icon}/><span className={dashboard.siderbarOption}>Kanban Board</span></Link>
+        <div draggable='true' className={sidebar.kanbanOption}>
+          <Link className={sidebar.a} to="/kanbanboard"><BsKanban color="white" size="2em" className={sidebar.icon}/><span className={sidebar.siderbarOption}>Kanban Board</span></Link>
         </div>
-        <div draggable='true' className={dashboard.kanbanOption}>
-          <Link className={sidebar.a} to="/checklist"><GoChecklist  color="white" size="2em" className={dashboard.icon}/><span className={dashboard.siderbarOption}>Check List</span></Link>
+        <div draggable='true' className={sidebar.kanbanOption}>
+          <Link className={sidebar.a} to="/checklist"><GoChecklist  color="white" size="2em" className={sidebar.icon}/><span className={sidebar.siderbarOption}>Check List</span></Link>
         </div>
-        <div draggable='true' className={dashboard.kanbanOption}>
-          <Link className={sidebar.a} to="/focus-session"><CgTimer  color="white" size="2em" className={dashboard.icon}/><span className={dashboard.siderbarOption}>Focus Session</span></Link>
+        <div draggable='true' className={sidebar.kanbanOption}>
+          <Link className={sidebar.a} to="/focus-session"><CgTimer  color="white" size="2em" className={sidebar.icon}/><span className={sidebar.siderbarOption}>Focus Session</span></Link>
         </div>
-        <div className={dashboard.settingoption}>
-          <Link className={sidebar.a} to="/settings"><IoMdSettings  color="white" size="2em" className={dashboard.icon}/><span className={dashboard.siderbarOption}>Settings</span></Link>
+        <div className={sidebar.settingoption}>
+          <Link className={sidebar.a} to="/settings"><IoMdSettings  color="white" size="2em" className={sidebar.icon}/><span className={sidebar.siderbarOption}>Settings</span></Link>
         </div>
       </div>
     </>
