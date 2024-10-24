cfe66df8752de4c9fc254991b30e517af49d61bd
KKA-0 <karanyobro@gmail.com>
2024-10-24 10:49:43 +0530

    Monolithic arch removed

--- Server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('./../controllers/authController')
-
-router.post('/token', authController.createToken)
-router.get('/token', authController.checkToken)
-
-module.exports = router
--- Server/routes/settingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const settingController = require('./../controllers/settingController')
-
-router.post('/settings/:user_id', settingController.addSettings)
-router.get('/settings/:user_id', settingController.getSettings)
-
-module.exports = router
